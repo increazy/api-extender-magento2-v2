50eafeb17709261dcbeca1bfbf40c2095dcbf25b
Daniel Valiati <daniel@tryideas.com.br>
2026-04-08 21:07:29 -0300

    feat: add webhooks for catalog and cart price rule events

---
+++ ApiExtenderV2/Observer/CatalogRuleSaveAfter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Increazy\ApiExtenderV2\Observer;
+
+use Increazy\ApiExtenderV2\Model\WebClientInterface;
+use Magento\Framework\Event\ObserverInterface;
+
+class CatalogRuleSaveAfter implements ObserverInterface
+{
+   /**
+     * @param WebClientInterface
+     */
+    private $webClient;
+
+    public function __construct(WebClientInterface $webClient)
+    {
+        $this->webClient = $webClient;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        try {
+            $rule = $observer->getRule();
+            $id = $rule->getId();
+            $this->webClient->initialize('catalogrule', __CLASS__);
+
+            $payload = [
+                'action' => 'save',
+                'entity' => $id,
+            ];
+
+            $this->webClient->pushWebhook($payload);
+        } catch (\Exception $e) {}
+    }
+}
